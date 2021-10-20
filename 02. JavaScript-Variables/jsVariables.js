0501c46df53f469b769aa1a7eb1cdadf6102e00b
unknown <yeasin.mohammod018@gmail.com>
2021-10-20 17:06:57 +0600

    Function added

---
+++ 02. JavaScript-Variables/jsVariables.js
@@ -0,0 +1,11 @@
+/*
+--> Used to store data temporarily
+--> Variable will not specific data type
+--> Variables can have global or local scope
+*/
+
+var name = "Injamul";
+var age = 25;
+var occupation = "Front-end Web Developer";
+
+document.write("NAME : "+ name +"</br>" + "AGE : "+ age +"</br>" + "Job: " +occupation);
---
+++ 03. JavaScript-Functions/function.js
@@ -0,0 +1,23 @@
+/*
+
+--> It is a set of statements perform specific task
+--> Can be invoked whenever code  execution is required
+--> Provides reusability 
+--> Makes the code modular
+--> Can pass values through parameters
+--> Can return value from a function using 'return' keyword
+
+*/
+
+function sayHello () {
+    document.querySelector('#sayHello').innerText = "Hello There!";
+}
+
+
+function sayHelloAgain (name, age) {
+
+    if(name == "" || age == "")
+    return;
+    document.querySelector('#message').innerText = (name + ' is ' + age + ' years old.');
+}
+
