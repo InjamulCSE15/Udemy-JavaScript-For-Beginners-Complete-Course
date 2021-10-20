6aa54eb023787d429fc5ee196c2992d6cb49b82e
unknown <yeasin.mohammod018@gmail.com>
2021-10-21 00:49:29 +0600

    for loop

--- 05. JavaScript-For-Loop/forLoop.js
+++ 05. JavaScript-For-Loop/forLoop.js
@@ -11,3 +11,42 @@ for(var x = 1; x <= 10; x++){
     text += "x = "+ x + "<br>";
 }
 document.getElementById('forLoop').innerHTML = text +"<br>";
+
+// Odd numbers:
+var odd = "";
+for(var i = 1; i <= 10; i+=2){
+    odd += "i = "+ i + "<br>";
+}
+document.getElementById('forLoopOdd').innerHTML = odd +"<br>";
+
+// Even numbers:
+var even = "";
+for(var y = 1; y <= 10; y++){
+    if( y % 2 == 0) {
+        even += "y = "+ y + "<br>";
+    }
+}
+document.getElementById('forLoopEven').innerHTML = even +"<br>";
+
+// Sum of Even numbers:
+var even = "";
+var sum = 0;
+for(var y = 1; y <= 10; y++){
+    if( y % 2 == 0) {
+        sum = sum + y;
+        even += "y = "+ y + "<br>";
+    }
+}
+document.getElementById('forLoopEvenSum').innerHTML = "Sum of even numbers from 1 to 10, Total: " +sum;
+
+// Factorial: 
+
+var a;
+var product = 1;
+var number = parseInt(prompt("Enter a number","Enter..."));
+
+for (a = 1; a <= number; a++) {
+    product = product * a;
+}
+
+document.querySelector('#factorial').innerHTML = "Factorial: " +product;
